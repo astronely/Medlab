be40d00d439d77e5426b963cae70303c2361258f
Krsn Nikita <nikborowik200394@gmail.com>
2024-10-15 18:59:52 +0500

    Complete home page

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header.jsx";
 import AboutUs from "../components/AboutUs.jsx";
 import Footer from "../components/Footer.jsx";
-import {useMediaQuery} from "react-responsive";
 
 export default function AboutUsPage() {
     return (
