65348076bed864712e648649ee8330202c18e125
Krsn Nikita <nikborowik200394@gmail.com>
2024-10-15 18:59:24 +0500

    Complete home page

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -4,8 +4,6 @@ import Footer from "../components/Footer.jsx";
 import {useMediaQuery} from "react-responsive";
 
 export default function AboutUsPage() {
-    const isMobile = useMediaQuery({query: '(max-width: 768px)'})
-
     return (
         <>
             <Header />
